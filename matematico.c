4a06cfab18fa92d18813f31a83dcca26e45b9268
Adejair Júnior <adejair.junioroulook@gmail.com>
2017-12-12 11:53:28 -0200

    Adicionado Jogo Matematico

---
+++ matematico.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Bem vindo ao Jogo de Raciocionio Logico Matematico !\n");
+    printf("Criadores: Adejair Junior & Kaique\n");
+
+    int result = 0;
+    int maxValue = 0;
+    double c;
+
+    printf("Digite o maior numero que pode ocorrer na sequencia do jogo: ex(20): (1+18) ou (18 /6)\n");
+    scanf("%i", &maxValue);
+
+    printf("VOCE PODE SAIR A QUALQUER MOMENTO DIGITANDO: -999\n");
+
+    char operacoes[4] = {'+', '-', '/', '*'};
+    do {
+        int rand1 = (rand() % maxValue) + 1;
+        int rand2 = (rand() % maxValue) + 1;
+
+        int op = rand() % 4;
+
+        printf("Qual o resultado de: %i %c %i: ", rand1, operacoes[op], rand2);
+        scanf("%i", &result);
+
+        switch(operacoes[op]) {
+            case '+': {
+                c = rand1 + rand2;
+                break;
+            }
+
+            case '-': {
+                c = rand1 - rand2;
+                break;
+            }
+
+            case '*': {
+                c = rand1 * rand2;
+                break;
+            }
+
+            case '/': {
+                c = rand1 / rand2;
+                break;
+            }
+        }
+
+
+        if(result == -999 && c != -999) {
+            printf("voce caiu fora do jogo !!");
+            break;
+        }
+
+
+        if(result == c) {
+         printf("Voce acertou o resultado!!\n ");
+        } else {
+            printf("Voce errou o resultado :(\n");
+        }
+
+    } while(result != -999);
+
+    return 0;
+}
