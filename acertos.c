d97308cc7271b6d61c11dc47266b168830706dec
Adejair Júnior <adejair.junioroulook@gmail.com>
2017-12-12 14:14:54 -0200

    Jogo dos acertos

---
+++ acertos.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int opt;
+        printf("===================================\n");
+        printf("Sistemas Operacionais - UNIRV\n");
+        printf("\tJogo dos acertos\n");
+        printf("Adejair Junior & Kaique\n ");
+        printf("====================================\n");
+        printf("DIGITE -999 PARA SAIR DO JOGO\n");
+        printf("ACERTE O NUMERO RANDOMICO GERADO NA TELA E VENCA O JOGO !!\n");
+
+        int intervalo;
+        int erros = 0;
+
+        printf("DIGITE O INTERVALO DO ACERTO 0-N\n");
+        scanf("%i", &intervalo);
+
+
+
+        int n = rand() % intervalo;
+
+        printf("%i\n", n);
+
+
+    do {
+
+        scanf("%i", &opt);
+
+
+        if(opt == n) {
+            printf("voce venceeeeeeeeu !!!");
+            printf("voce errou %i vezes antes de acertar\n", erros);
+            break;
+        }if(opt < n) {
+            printf("O numero eh menorrr\n");
+            ++erros;
+        } else {
+            printf("o numero eh maiorr\n");
+            ++erros;
+        }
+
+
+    } while(opt != -999 || opt == n);
+
+    system("clear");
+    system("./menu");
+    return 0;
+}
