9364b25fea868f015569bae90e603857e73cf724
Adejair Júnior <adejair.junioroulook@gmail.com>
2017-12-12 13:11:57 -0200

    Criado um Menu

---
+++ menu.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int opt;
+
+    do {
+
+
+        printf("============= MENU ================\n");
+        printf("Trabalho de Sistemas Operacionais \n");
+        printf("Alunos: Adejair Junior & Kaique \n");
+        printf("====================================\n");
+        printf("Escolha o Jogo para ser executado:\n");
+        printf("1) Jogo de Raciocionio Matematico\n");
+        scanf("%i", &opt);
+
+        switch(opt) {
+            case 1: {
+                system("gcc matematico -o matematico.c");
+                system("./matematico");
+                break;
+            }
+        }
+
+    } while((opt < 1) || (opt > 2));
+    return 0;
+}
